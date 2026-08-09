934db078464dd09b4dea74ff59b1dc9957950681
Funck <ragrafx19-cloud@example.com>
2026-08-09 00:30:40 -0400

    Switch database to PostgreSQL

--- app/database.py
+++ app/database.py
@@ -1,20 +1,16 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True
-)
+# IMPORTANT: PostgreSQL needs this argument
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-
-# Dependency for FastAPI routes
 def get_db():
     db = SessionLocal()
     try:
